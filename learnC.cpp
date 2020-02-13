75f76db396cce5ff7b52e080d411566a3174ddec
kun jiang <jackquinn@foxmail.com>
2020-02-13 23:50:14 +0800

    NEW

--- learnC.cpp
+++ learnC.cpp
@@ -1,46 +1,28 @@
-﻿// learnC.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <stdio.h>
 
-#include <iostream>
-using namespace std;
-
-class Line
-{
-public:
-	Line(double len);
-	void setL(double leng);
-	double getL(void);
-private:
-	double length;
-};
-Line::Line(double len)
-{
-	length = len;
-}
-void Line::setL(double leng)
-{
-	length = leng;
-}
-double Line::getL(void)
-{
-	return length;
-}
 int main()
 {
-	Line line(1.0);
-	cout << line.getL() << endl;
-	line.setL(8.2);
-	cout << line.getL() << endl;
-	return 0;
+/*    int* p = new int;
+
+    *p = 5;
+    *p = *p + 10;
+
+    printf("p = %p\n", p);
+    printf("*p = %d\n", *p);
+
+    delete p;*/
+
+    int *p = new int[10];
+
+    for (int i = 0; i < 10; i++)
+    {
+        p[i] = i + 1;
+
+        printf("p[%d] = %d\n", i, p[i]);
+    }
+
+    delete[] p;
+
+    return 0;
+
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
