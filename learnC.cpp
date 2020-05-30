1753697e3bcfc93634456e4ebc3a197e2b364de2
kun jiang <jackquinn@foxmail.com>
2020-05-30 23:32:27 +0800

    seq table
    seq table P18 Q10

--- learnC.cpp
+++ learnC.cpp
@@ -1,12 +1,26 @@
 ﻿#include<iostream>
 #include<stdio.h>
-#include"Point.h"
+
 using namespace std;
 int main()
 {
-	Point a[3];
-	Point* p = a;
-	(p+1)->setxy(6.6, 8.8);
-	(p + 1)->show();
+	int r[10] = { 1,2,3,4,5,6,7,8,9,10};
+	int p;
+	cin >> p;
+	int* t = new int[p];
+	
+	for (int i = 0;i <= p-1;i++)
+		t[i] = r[i]; 
+
+	for (int j = p;j <= 9;j++)
+		r[j - p] = r[j]; 
+
+	for (int m = 0;m <= p - 1;m++)
+		r[m+10-p]=t[m];
+
+	for(int k=0;k<=9;k++)
+	cout << r[k] << endl;
+	
 	return 0;
+	
 }
