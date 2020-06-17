38794a551404f9bc37aec467655bda74bab2fcce
kun jiang <jackquinn@foxmail.com>
2020-06-18 00:20:53 +0800

    LNode
    LNode *p=NULL;
    p=listHI(p);

--- learnC.cpp
+++ learnC.cpp
@@ -3,26 +3,49 @@
 #include<stdlib.h>
 using namespace std;
 
+typedef int elemtype;
+typedef struct LNode
+{
+    elemtype data;
+    struct LNode* next;
+}LNode;
+
+LNode *listHI(LNode* L);
+LNode* LocateElem(LNode*L, elemtype e);
+
 int main()
 {
-	typedef int elemtype;
-	typedef struct LNODE
-	{
-		elemtype data;
-		struct LNODE *next;
-	}link,*linklist;
-	link *p;
-	link* head = new link;
-	head->next = NULL;
-	p = head;
+    LNode *p = NULL; //这里为什么必须NULL?
+    p=listHI(p);   //注意，光listHI(p)没用，只是return给p了，然而要赋给函数里的p，才有意义。函数里的p本质上是一个指针，指向头结点的。
+    int x = 4;
+    cout << p->next->data << endl;
+    cout << p->next->next->data << endl;
+    cout << LocateElem(p, x)->data << endl;
+    return 0;
+}
 
-	for (int i = 1;i <= 10;i++)
-	{
-		link* a = new link;
-		a->data = i;
-		a->next = p->next;
-		p->next=a;
-	}
-	cout << p->next->next->data;
-	return 0;
+LNode* listHI(LNode* L)
+{
+    LNode* s;int x;
+    L = new LNode;
+    L->next = NULL;
+    cin >> x;
+    while (x != 9999)
+    {
+        s = new LNode;
+        s->data = x;
+        s->next = L->next;
+        L->next = s;
+        cin >> x;
+    }
+    return L;
+}
+
+LNode* LocateElem(LNode* L, elemtype e)
+{
+    LNode* p = L->next;
+    while (p != NULL && p->data != e)
+        p = p->next;
+    return p;
+
 }
